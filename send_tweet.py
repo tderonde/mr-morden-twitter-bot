b99f0293a45963532e4653058e444fca8e248533
tderonde <80601138+tderonde@users.noreply.github.com>
2022-07-08 08:27:56 -0700

    Update send_tweet.py

--- send_tweet.py
+++ send_tweet.py
@@ -3,11 +3,13 @@ from tweepy_client import client, api
 # send tweet or reply to user's tweet using twitter api 
 def send_tweet(text, tweet_id=None, media_id=None):
     print(f"Running {send_tweet.__name__}({text, tweet_id, media_id})...")
+    if media_id is not None:
+        media_id = [media_id]
     try:
         response = client.create_tweet(
             text=text
             , in_reply_to_tweet_id = tweet_id
-            , media_ids=[media_id]
+            , media_ids=media_id
         )
         tweet_link = f"Tweet created: https://twitter.com/user/status/{response.data['id']}"
         print(tweet_link)
