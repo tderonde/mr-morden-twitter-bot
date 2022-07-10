04c08c8b73af298f5f535ede2435f0fc385dba14
tderonde <80601138+tderonde@users.noreply.github.com>
2022-07-10 11:46:17 -0700

    Add files via upload

--- get_user_id.py
+++ get_user_id.py
@@ -2,14 +2,19 @@ from tweepy_client import client
 
 # get user id from twitter api
 def get_user_id(user_name):
-    print(f"Running {get_user_id.__name__}({user_name})...")
-    response = client.get_users(usernames=user_name)
-    try:
-        user_id = response.data[0].id
-        print(f"Success. User ID {user_id} found.")
-    except:
-        user_id = None
-        print(f"Error.")
+    user_id = None
+    i = 0
+    while user_id == None and i < 5:
+        print(f"Running {get_user_id.__name__}({user_name})...")
+        response = client.get_users(usernames=user_name)
+        try:
+            user_id = response.data[0].id
+            print(f"Success. User ID {user_id} found.")
+        except:
+            user_id = None
+            print(f"Error.")
+        i += 1
+    
     return user_id
 
 if __name__ == "__main__":
