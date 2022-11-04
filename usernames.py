c7ae3835f4aa21b140a6f19ab4020dde73f28480
tderonde <80601138+tderonde@users.noreply.github.com>
2022-11-03 21:48:48 -0700

    Update usernames.py

--- usernames.py
+++ usernames.py
@@ -12,5 +12,7 @@ user_names = list(twitters.loc[twitters['party'] == 'R']['username'].values)
 # user_names.append('B5News')
 # append JMS twitter
 user_names.append('straczynski')
+# append Musk twitter to list to make up 1/4th of list
+user_names = user_names + ['elonmusk'] * int(len(user_names / 4))
 
 ## next steps: add MrMordenBot followers/following to list ##
